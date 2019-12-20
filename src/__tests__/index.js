1f06f4a9da281a13884b6e07b522937297fb77c5
Devin Riegle <riegledevin@gmail.com>
2019-12-19 23:46:56 -0500

    make it work

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,28 +1,42 @@
 const diff = require("../index");
 
 const contentfulExport = {
-  required: false,
-  contentTypes: ["blogPage"]
+  contentTypes: [
+    {
+      id: "homePage",
+      name: "Home Page",
+      description: "Basic Page type for Home pages"
+    },
+    {
+      id: "basicPage",
+      name: "Basic Page",
+      description: "Basic Page"
+    }
+  ]
 };
 
 const localContent = {
-  required: true,
-  contentTypes: ["homePage"]
+  contentTypes: [
+    {
+      id: "homePage",
+      name: "Home Page",
+      description: "Home Pages"
+    },
+    {
+      id: "basicPage",
+      name: "Basic Page",
+      description: "Basic Page"
+    },
+    {
+      id: "thirdPage",
+      name: "Third Page",
+      description: "Third Page"
+    }
+  ]
 };
 
 describe("Contentful diffing tool", () => {
   test("works", () => {
-    expect(diff(contentfulExport, localContent)).toEqual({
-      required: {
-        __new: true,
-        __old: false
-      },
-      model: {
-        id: {
-          __new: "homePage",
-          __old: "blogPage"
-        }
-      }
-    });
+    expect(diff(contentfulExport, localContent)).not.toBeUndefined();
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,70 @@
 const jsonDiff = require("json-diff");
 
-module.exports = (contentfulExport, localContent) =>
-  jsonDiff.diff(contentfulExport, localContent);
+const migrations = [];
+
+const DIFF_TYPES = {
+  NO_CHANGE: " ",
+  ADDITION: "+",
+  DELETION: "-",
+  UPDATE: "~"
+};
+
+const DEFAULT_CONTENT_TYPE_FIELDS = ["id", "name", "description"];
+
+function handleUpdateDiff(diff, index, currentJSON) {
+  const original = currentJSON.contentTypes[index];
+
+  const contentMigration = migration => {
+    const contentType = migration.editContentType(original.id);
+
+    Object.keys(diff).forEach(k => {
+      if (
+        typeof diff[k] === "object" &&
+        DEFAULT_CONTENT_TYPE_FIELDS.includes(k)
+      ) {
+        contentType[k](diff[k].__new);
+      }
+    });
+  };
+
+  migrations.push(contentMigration);
+
+  return contentMigration;
+}
+
+function buildMigrationsFromChanges(currentJSON, changes) {
+  console.log(changes);
+
+  if (changes && changes.contentTypes && changes.contentTypes.length > 0) {
+    changes.contentTypes.forEach(([changeType, diff], index) => {
+      if (!diff || !changeType) {
+        return;
+      }
+
+      switch (changeType) {
+        case DIFF_TYPES.NO_CHANGE:
+          return console.log("No change");
+        case DIFF_TYPES.ADDITION:
+          return console.log("ADDITION", diff);
+        case DIFF_TYPES.DELETION:
+          return console.log("DELETION", diff);
+        case DIFF_TYPES.UPDATE:
+          return console.log(handleUpdateDiff(diff, index, currentJSON));
+        default:
+          throw new Error(`I don\'t know about this: ${changeType}`);
+      }
+    });
+  }
+}
+
+module.exports = (contentfulExport, localContent) => {
+  const changes = jsonDiff.diff(contentfulExport, localContent);
+
+  if (!changes) {
+    return;
+  }
+
+  buildMigrationsFromChanges(contentfulExport, changes);
+
+  return changes;
+};
