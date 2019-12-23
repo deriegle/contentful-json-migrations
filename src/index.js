71fc9657b895dc365e3e81d84f73b73cc7294353
Devin Riegle <riegledevin@gmail.com>
2019-12-23 16:12:10 -0500

    move to one index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
+
 const MigrationGeneratorService = require("./models/migration-generator-service");
 const ContentfulDiffService = require("../src/models/contentful-diff-service");
+const fs = require("fs");
 
-module.exports = (contentfulExport, localContent) => {
+function App(contentfulExport, localContent) {
   const changes = ContentfulDiffService.generateDiff(
     contentfulExport,
     localContent
@@ -12,4 +15,48 @@ module.exports = (contentfulExport, localContent) => {
   }
 
   new MigrationGeneratorService(localContent).build(changes);
-};
+}
+
+require("yargs")
+  .scriptName("Contentful JSON migrations")
+  .usage("$0 <cmd> [args]")
+  .command(
+    "create:migrations [filePath]",
+    "Create migrations from diff in JSON file",
+    yargs => {
+      yargs.positional("filePath", {
+        type: "string",
+        default: "./migrations.json",
+        describe: "the file path to the JSON migration file"
+      });
+    },
+    function(argv) {
+      const path = argv.filePath;
+
+      if (!fs.existsSync(path)) {
+        throw new Error("Path is invalid.");
+      }
+
+      if (!path.endsWith(".json")) {
+        throw new Error("File must be a JSON file");
+      }
+
+      try {
+        const currentLocalMigrations = require(path);
+
+        // Get from Contentful using CLI (Downloading to JSON file) or get using API
+        const currentContentfulMigrations = {
+          contentTypes: []
+        };
+
+        App(currentContentfulMigrations, currentLocalMigrations);
+      } catch (e) {
+        console.error(e);
+
+        throw new Error("Could not parse JSON file");
+      }
+    }
+  )
+  .help().argv;
+
+module.exports = App;
